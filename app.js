8ebe1e9972f57d97b7f34c698d9e3d06e2388c3e
Nazmul Hasan <you@example.com>
2022-04-28 01:35:05 +0600

    upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const student = [
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        Skill : 'MERN',
+    },
+    {
+        Name : 'Asraful',
+        Age : 25,
+        Email : 'asraful@gmail.com',
+        Skill : 'IOS Developer',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+    {
+        Name : 'Nazmul Huda',
+        Age : 25,
+        Email : 'nazmulhasantamim1@gmail.com',
+        ClassName : 'MERN',
+    },
+];
+
+console.log(student);
